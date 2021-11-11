49464b92b0e358a62394d12bdb5c076fb2e57f93
Caleb Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2021-11-11 21:03:13 +0000

    Modified useAboutQuery.js

--- src/hooks/useAboutQuery.js
+++ src/hooks/useAboutQuery.js
@@ -4,7 +4,6 @@ export const useAboutQuery = () => {
   const data = useStaticQuery(graphql`
     query {
       wpPage(databaseId: { eq: 47 }) {
-        id
         content
         featuredImage {
           node {
