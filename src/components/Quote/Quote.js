e92d5750841a3b0b7a0ea79722725d62b964a898
Caleb Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2021-11-11 10:09:58 +0000

    Added Quote.js

---
+++ src/components/Quote/Quote.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { useQuoteQuery } from "../../hooks/useQuoteQuery"
+import QuoteImg from "../../images/quote.svg"
+import { Wrapper, Content, QuoteHover } from "./Quote.styles"
+
+const Quote = () => {
+  const {
+    wpPage: { ACF_HomePage: data },
+  } = useQuoteQuery()
+  console.log(data)
+
+  return (
+    <Wrapper>
+      <Content>
+        <QuoteHover src={QuoteImg} alt="quote" />
+        <h6>{data.citat1Text}</h6>
+        <p>{data.citat1Author}</p>
+      </Content>
+    </Wrapper>
+  )
+}
+
+export default Quote
